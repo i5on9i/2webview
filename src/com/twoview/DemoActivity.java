4de2b92d3e97bf85b45ce5185c10d6db7d82f4b3
i5on9i <gaedduck@gmail.com>
2013-07-24 14:56:47 +0900

    - add isDragViewTipHit() - only tip part of sliding view can be clicked.

---
+++ src/com/twoview/DemoActivity.java
@@ -0,0 +1,137 @@
+package com.twoview;
+
+
+
+
+import com.twoview.SlidingUpPanelLayout.PanelSlideListener;
+
+import android.net.http.SslError;
+import android.os.Build;
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+public class DemoActivity extends Activity {
+
+	private String mainUrl = "http://www.economist.com/printedition";
+	private String subUrl = "http://m.endic.naver.com/";
+	
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        setContentView(R.layout.activity_demo);
+
+        SlidingUpPanelLayout layout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        layout.setShadowDrawable(getResources().getDrawable(R.drawable.above_shadow));
+        layout.setPanelSlideListener(new PanelSlideListener() {
+
+            @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+			@Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                if (slideOffset < 0.2) {
+                    if (getActionBar().isShowing()) {
+                        getActionBar().hide();
+                    }
+                } else {
+                    if (!getActionBar().isShowing()) {
+                        getActionBar().show();
+                    }
+                }
+            }
+
+            @Override
+            public void onPanelExpanded(View panel) {
+            	WebView mWebView = (WebView) findViewById(R.id.webview2);
+        		
+        		WebSettings webSettings = mWebView.getSettings();
+        		webSettings.setJavaScriptEnabled(true);
+        		
+        		
+        		mWebView.setWebViewClient(new MyWebViewClient());
+        		
+        		mWebView.loadUrl(subUrl);
+
+            }
+
+            @Override
+            public void onPanelCollapsed(View panel) {
+
+
+            }
+        });
+        TextView t = (TextView) findViewById(R.id.brought_by);
+        t.setMovementMethod(LinkMovementMethod.getInstance());
+        
+        
+        
+        WebView mWebView = (WebView) findViewById(R.id.webview1);
+		
+		WebSettings webSettings = mWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		
+		
+		mWebView.setWebViewClient(new MyWebViewClient());
+		
+		mWebView.loadUrl(mainUrl);
+		
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.demo, menu);
+        return true;
+    }
+    
+    ////////////////////////////////////////////////
+    ////
+    ////    private MyWebViewClient 
+    ////
+    private class MyWebViewClient extends WebViewClient {
+    	@Override
+    	public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+    		// This is my web site, so do not override; let my WebView load the page
+    		Log.d("namh", url);
+    		view.loadUrl(url);
+            return false; // then it is not handled by default action
+
+    	}
+
+    	@Override
+    	public void onPageStarted(WebView view, String url, Bitmap favicon) {
+
+//    		if(url.startsWith(PocketVariables.REDIRECT_URL)){
+//
+//    			Log.d("namh", "login has been finished");
+//
+//    		}
+    	}
+    	
+    	 @Override
+    	 public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+    		 super.onReceivedSslError(view, handler, error);
+
+    		 // this will ignore the Ssl error and will go forward to your site
+    		 handler.proceed();
+    	 }
+
+
+    }
+
+}
